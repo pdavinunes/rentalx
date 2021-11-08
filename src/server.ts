72a47beb87f84d6af0f74a45ef498b5c2c9bdf41
pdavinunes <p.davi.nunes@gmail.com>
2021-11-08 20:26:17 -0300

    add inspeção via debug no vscode

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World Ignite' });
-});
+app.use(express.json());
 
 app.listen(3333, () => console.log('Server is runnig!'));
