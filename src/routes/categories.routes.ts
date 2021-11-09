d34185bb80d314687194cf7decd992659fba46b8
pdavinunes <p.davi.nunes@gmail.com>
2021-11-08 21:10:33 -0300

    realiza listagem de categorias

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -13,4 +13,10 @@ categoriesRoutes.post('/', (request, response) => {
   return response.status(201).send();
 });
 
+categoriesRoutes.get('/', (request, response) => {
+  const categories = categoriesRepository.list();
+
+  return response.json(categories);
+});
+
 export { categoriesRoutes };
