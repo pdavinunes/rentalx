e40db09ee3217d45ae2e138b5b2018a14e06f4cb
pdavinunes <p.davi.nunes@gmail.com>
2021-11-09 21:49:30 -0300

    add funcionalidade de criação de especificação

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,33 @@
+import { Specification } from '../model/Specification';
+import {
+  ICreateSpecificationDTO,
+  ISpecificationRepository,
+} from './ISpecificationRepository';
+
+class SpecificationsRepository implements ISpecificationRepository {
+  private specifications: Specification[];
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      specification => specification.name === name,
+    );
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 
-// import { CategoriesRepository } from '../repositories/CategoriesRepository';
-import { PostgresCategoryRepository } from '../modules/cars/repositories/PostgresCategoriesRepository';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
 import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new PostgresCategoryRepository();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
