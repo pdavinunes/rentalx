b3bf25017e4a5c9c63296482f94a8b73591572f9
pdavinunes <p.davi.nunes@gmail.com>
2021-11-09 21:23:29 -0300

    cria estrutura de especificações e modulo de carros

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
-// import { PostgresCategoryRepository } from '../repositories/PostgresCategoriesRepository';
-import { CreateCategoryService } from '../services/CreateCategoryService';
+// import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { PostgresCategoryRepository } from '../modules/cars/repositories/PostgresCategoriesRepository';
+import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = new PostgresCategoryRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
