a5ca6bba31f0a6ff89594ca40e6f56413bf500da
pdavinunes <p.davi.nunes@gmail.com>
2022-02-12 18:03:31 -0300

    feat(categories): add rota de importação de arquivo

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { createCateogoryController } from '../modules/cars/useCases/createCateogory';
+import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({ dest: './tmp' });
 
 categoriesRoutes.post('/', (request, response) => {
   return createCateogoryController.handle(request, response);
@@ -11,6 +15,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
