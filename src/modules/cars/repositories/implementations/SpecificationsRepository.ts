d25a8415aa2fe9ff4d6b88851b7a170298061cd3
pdavinunes <p.davi.nunes@gmail.com>
2022-02-12 17:15:32 -0300

    feat(specification): aplica padrão sigleton no repository

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -6,10 +6,19 @@ import {
 
 class SpecificationsRepository implements ISpecificationRepository {
   private specifications: Specification[];
-  constructor() {
+
+  private static INSTANCE: SpecificationsRepository;
+
+  private constructor() {
     this.specifications = [];
   }
 
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+    return SpecificationsRepository.INSTANCE;
+  }
   create({ name, description }: ICreateSpecificationDTO): void {
     const specification = new Specification();
 
@@ -23,10 +32,9 @@ class SpecificationsRepository implements ISpecificationRepository {
   }
 
   findByName(name: string): Specification {
-    const specification = this.specifications.find(
-      specification => specification.name === name,
+    return this.specifications.find(
+      specificationItem => specificationItem.name === name,
     );
-    return specification;
   }
 }
 
