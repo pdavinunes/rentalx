723a4dbce62b395001999dc700b3e0c298f7dce4
pdavinunes <p.davi.nunes@gmail.com>
2022-05-09 22:04:53 -0300

    feat: utiliza stream para realizar leitura de csv

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,20 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  constructor(private categoriesRepository: ICategoriesRepository) { }
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile);
+
+    parseFile.on('data', async line => {
+      console.log(line);
+    });
   }
 }
 
