11d0d4b19482826163424837020083095139a19b
pdavinunes <p.davi.nunes@gmail.com>
2022-05-12 23:00:43 -0300

    feat: implementa lógica do caso de uso de importação

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -3,18 +3,52 @@ import { parse } from 'csv-parse';
 
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) { }
-  execute(file: Express.Multer.File): void {
-    const stream = fs.createReadStream(file.path);
 
-    const parseFile = parse();
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
 
-    stream.pipe(parseFile);
+      const categories: IImportCategory[] = [];
 
-    parseFile.on('data', async line => {
-      console.log(line);
-    });
+      const parseFile = parse();
+
+      stream.pipe(parseFile);
+
+      parseFile.on('data', async line => {
+        const [name, description] = line;
+        categories.push({
+          name,
+          description
+        })
+      }).on('end', () => {
+        resolve(categories);
+      }).on('error', (err) => {
+        reject(err)
+      })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.forEach(async (category) => {
+      const { name, description } = category;
+      const existsCategory = this.categoriesRepository.findByName(name);
+
+      if (!existsCategory) {
+        this.categoriesRepository.create({
+          name,
+          description
+        })
+      }
+    })
   }
 }
 
