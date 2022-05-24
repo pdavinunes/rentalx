17f14583f8ddb4570fd14c3663702dbb93b5a331
pdavinunes <p.davi.nunes@gmail.com>
2022-05-24 20:48:36 -0300

    feat(categoria): atualiza rotas

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,24 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import { createCateogoryController } from '../modules/cars/useCases/createCateogory';
-import { importCategoryController } from '../modules/cars/useCases/importCategory';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import createCateogoryController from '../modules/cars/useCases/createCateogory';
+import importCategoryController from '../modules/cars/useCases/importCategory';
+import listCategoriesController from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
 
 const upload = multer({ dest: './tmp' });
 
 categoriesRoutes.post('/', (request, response) => {
-  return createCateogoryController.handle(request, response);
+  return createCateogoryController().handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  return listCategoriesController.handle(request, response);
+  return importCategoryController().handle(request, response);
 });
 
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  return importCategoryController.handle(request, response);
+  return listCategoriesController().handle(request, response);
 });
 
 export { categoriesRoutes };
