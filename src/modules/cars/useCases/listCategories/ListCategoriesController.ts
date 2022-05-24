79ec375c214b3061d8d8714a566b46ec96905dc6
pdavinunes <p.davi.nunes@gmail.com>
2022-05-24 20:58:54 -0300

    chore: remove erro do sonarlint

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -4,7 +4,7 @@ import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
   constructor(private listCategoriesUseCase: ListCategoriesUseCase) { }
-  async handle(request: Request, response: Response): Promise<Response> {
+  async handle(_request: Request, response: Response): Promise<Response> {
     const categories = await this.listCategoriesUseCase.execute();
 
     return response.json(categories);
