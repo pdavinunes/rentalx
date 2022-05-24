d75ace9281145f2f204f8d78ec5a6c30aa86c436
pdavinunes <p.davi.nunes@gmail.com>
2022-05-24 20:48:25 -0300

    feat(categoria): atualiza index de caso de uso de importação

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,11 +1,11 @@
-import {CategoriesRepository} from '../../repositories/implementations/CategoriesRepository';
-import {ImportCategoryController} from './ImportCategoryController';
-import {ImportCategoryUseCase} from './ImportCategoryUseCase';
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+import { ImportCategoryController } from './ImportCategoryController';
+import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
-const categoriesRepository = CategoriesRepository.getInstance();
-const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
-const importCategoryController = new ImportCategoryController(
-  importCategoryUseCase
-);
-
-export {importCategoryController};
+export default (): ImportCategoryController => {
+  const categoriesRepository = new CategoriesRepository();
+  const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
+  return new ImportCategoryController(
+    importCategoryUseCase
+  );
+}
