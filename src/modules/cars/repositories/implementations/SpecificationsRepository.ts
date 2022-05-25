3fc310528519b3269380179cf5018cdcb04d6ab0
pdavinunes <p.davi.nunes@gmail.com>
2022-05-25 20:08:39 -0300

    feat(especificaoes): aplica typeorm a implementação do repository

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,3 +1,5 @@
+import { Repository } from 'typeorm';
+import dataSource from '../../../../database/data-source';
 import { Specification } from '../../entities/Specification';
 import {
   ICreateSpecificationDTO,
@@ -5,36 +7,24 @@ import {
 } from '../ISpecificationRepository';
 
 class SpecificationsRepository implements ISpecificationRepository {
-  private specifications: Specification[];
 
-  private static INSTANCE: SpecificationsRepository;
+  private repository: Repository<Specification>
 
-  private constructor() {
-    this.specifications = [];
+  constructor() {
+    this.repository = dataSource.getRepository(Specification);
   }
 
-  public static getInstance(): SpecificationsRepository {
-    if (!SpecificationsRepository.INSTANCE) {
-      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
-    }
-    return SpecificationsRepository.INSTANCE;
-  }
-  create({ name, description }: ICreateSpecificationDTO): void {
-    const specification = new Specification();
-
-    Object.assign(specification, {
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
       name,
-      description,
-      created_at: new Date()
-    });
+      description
+    })
 
-    this.specifications.push(specification);
+    await this.repository.save(specification);
   }
 
-  findByName(name: string): Specification {
-    return this.specifications.find(
-      specificationItem => specificationItem.name === name
-    );
+  async findByName(name: string): Promise<Specification> {
+    return this.repository.findOneBy({ name })
   }
 }
 
