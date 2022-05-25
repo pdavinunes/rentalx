04443c8c12e430f7f14520fdc4f6d1f3e315cc56
pdavinunes <p.davi.nunes@gmail.com>
2022-05-25 20:10:10 -0300

    feat(especificação): refatora caso de uso de criação

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,25 +1,29 @@
-import {ISpecificationRepository} from '../../repositories/ISpecificationRepository';
+import { inject, injectable } from 'tsyringe';
+import { ISpecificationRepository } from '../../repositories/ISpecificationRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
+@injectable()
 class CreateSpecificationUseCase {
-  constructor(private specificationsRepository: ISpecificationRepository) {}
+  constructor(
+    @inject("SpecificationsRepository")
+    private specificationsRepository: ISpecificationRepository) { }
 
-  execute({name, description}: IRequest): void {
+  async execute({ name, description }: IRequest): Promise<void> {
     const specificationAlreadyExists =
-      this.specificationsRepository.findByName(name);
+      await this.specificationsRepository.findByName(name);
 
     if (specificationAlreadyExists) {
       throw new Error('Specification already exists!');
     }
 
-    this.specificationsRepository.create({
+    await this.specificationsRepository.create({
       name,
       description
     });
   }
 }
 
-export {CreateSpecificationUseCase};
+export { CreateSpecificationUseCase };
