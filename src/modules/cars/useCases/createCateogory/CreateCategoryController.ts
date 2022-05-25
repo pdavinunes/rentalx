00dded5d9a43bbb7dae7c0f182213447186b5acf
pdavinunes <p.davi.nunes@gmail.com>
2022-05-25 20:09:17 -0300

    feat(categoria): refatora caso de uso de criação

--- src/modules/cars/useCases/createCateogory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCateogory/CreateCategoryController.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express';
-
+import { container } from 'tsyringe'
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 class CreateCateogoryController {
-  constructor(private createCategoryUseCase: CreateCategoryUseCase) { }
+
   async handle(request: Request, response: Response) {
     const { name, description } = request.body;
 
-    await this.createCategoryUseCase.execute({ name, description });
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
+
+    await createCategoryUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
