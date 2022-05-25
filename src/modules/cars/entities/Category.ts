c02da54994211e21e904d7aea19b393a2fee03db
pdavinunes <p.davi.nunes@gmail.com>
2022-05-25 20:06:19 -0300

    formata entitidade de categorias

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,18 +1,18 @@
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
-import {v4 as uuidV4} from 'uuid';
+import { v4 as uuidV4 } from 'uuid';
 
 @Entity("categories")
 class Category {
-  
+
   @PrimaryColumn()
   id?: string;
 
   @Column()
   name: string;
-  
+
   @Column()
   description: string;
-  
+
   @CreateDateColumn()
   created_at: Date;
 
@@ -23,4 +23,4 @@ class Category {
   }
 }
 
-export {Category};
+export { Category };
