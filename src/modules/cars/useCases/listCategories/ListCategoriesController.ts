077fae80e46b5e0afd3f903e9542d85a58966b30
pdavinunes <p.davi.nunes@gmail.com>
2022-05-25 20:09:54 -0300

    feat(categoria): refatora caso de uso de listagem

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) { }
   async handle(_request: Request, response: Response): Promise<Response> {
-    const categories = await this.listCategoriesUseCase.execute();
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
+    const categories = await listCategoriesUseCase.execute();
 
     return response.json(categories);
   }
