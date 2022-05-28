0f1c8bfb339a5426ddcfdc9ebb34a718823febab
pdavinunes <p.davi.nunes@gmail.com>
2022-05-28 12:48:54 -0300

    feat(usuario): add caso de uso de criação

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from "tsyringe";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+import { hash } from 'bcryptjs'
+
+@injectable()
+class CreateUserUseCase {
+
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name, email, password, driver_license
+    })
+  }
+}
+
+export { CreateUserUseCase }
