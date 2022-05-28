bb33b65a710be828c9fdb8650dec07c7110736dc
pdavinunes <p.davi.nunes@gmail.com>
2022-05-28 12:46:58 -0300

    feat(usuario): add repositorio

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,27 @@
+import { Repository } from "typeorm";
+import dataSource from "../../../../database/data-source";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>
+
+  constructor() {
+    this.repository = dataSource.getRepository(User);
+  }
+
+  async create({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      driver_license,
+      password
+    })
+
+    await this.repository.save(user);
+  }
+
+}
+
+export { UsersRepository }
