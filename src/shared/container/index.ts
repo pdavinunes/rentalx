24d211640919add7c38c5a324757d765618ac1a0
pdavinunes <p.davi.nunes@gmail.com>
2022-05-28 12:48:02 -0300

    feat(usuario): singleton de repositorio

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,9 @@ import { CategoriesRepository } from '../../modules/cars/repositories/implementa
 import { ISpecificationRepository } from '../../modules/cars/repositories/ISpecificationRepository'
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository'
 
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository'
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository'
+
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
@@ -14,4 +17,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 )
