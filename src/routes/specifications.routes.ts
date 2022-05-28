f2a5ec6fd777c432aea90bee93b2892a4b55ef7f
pdavinunes <p.davi.nunes@gmail.com>
2022-05-28 14:05:50 -0300

    feat(especificação): aplica middleware na criacao

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const createSpecificationController = new CreateSpecificationController();
 
 const specificationsRoutes = Router();
+
+specificationsRoutes.use(ensureAuthenticated)
 
 specificationsRoutes.post('/', createSpecificationController.handle);
 
