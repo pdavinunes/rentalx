7e73497aa7c57ee21429a9fa61a7db347534574f
pdavinunes <p.davi.nunes@gmail.com>
2022-06-11 19:41:30 -0300

    refatora paths nos repositories in-memory

--- src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -1,5 +1,6 @@
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
-import { User } from "@modules/accounts/infra/typeorm/User";
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 class UsersRepositoryInMemory implements IUsersRepository {
