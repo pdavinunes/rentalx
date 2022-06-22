870e21fb69b70f7fe22caee1908d4a6ff6783d59
pdavinunes <p.davi.nunes@gmail.com>
2022-06-21 22:51:53 -0300

    feat(carro-espeficicacao): implementa caso de uso de criação

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -0,0 +1,42 @@
+import { inject, injectable } from "tsyringe";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
+import { AppError } from "@shared/errors/AppError";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+
+interface IRequest {
+  car_id: string;
+  specifications_id: string[];
+}
+
+@injectable()
+class CreateCarSpecificationUseCase {
+
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository,
+
+    @inject("SpecificationsRepository")
+    private specificationsRepository: ISpecificationRepository
+  ) { }
+
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
+    const carExists = await this.carsRepository.findById(car_id)
+
+    if (!carExists) {
+      throw new AppError("Car does not exists!")
+    }
+
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id
+    );
+
+    carExists.specifications = specifications
+
+    await this.carsRepository.create(carExists);
+
+    return carExists;
+  }
+}
+
+export { CreateCarSpecificationUseCase }
