9eed2a3045e290c73e94d452cfe3459ebca848c3
pdavinunes <p.davi.nunes@gmail.com>
2022-06-22 20:56:54 -0300

    build(usuario): fix na pk da tabela

--- src/shared/infra/typeorm/migrations/1653520362054-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1653520362054-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1653520362054 implements MigrationInterface {
                 {
                     name: 'id',
                     type: 'uuid',
+                    isPrimary: true
                 },
                 {
                     name: 'name',
