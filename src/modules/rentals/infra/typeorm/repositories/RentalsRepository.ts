60118b5b0f8bd4cb627e64bbef8cdef0ab9b57b8
pdavinunes <p.davi.nunes@gmail.com>
2022-06-27 22:28:34 -0300

    chore(aluguel): add implementação de repository

---
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -0,0 +1,38 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import dataSource from "@shared/infra/typeorm/data-source";
+import { singleton } from "tsyringe";
+import { Repository } from "typeorm";
+import { Rental } from "../entities/Rental";
+
+@singleton()
+class RentalsRepository implements IRentalsRepository {
+
+  private repository: Repository<Rental>
+
+  constructor() {
+    this.repository = dataSource.getRepository(Rental)
+  }
+
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    return this.repository.findOneBy({ car_id })
+  }
+
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    return this.repository.findOneBy({ user_id })
+  }
+
+  async create({ car_id, expected_return_date, user_id }: ICreateRentalDTO): Promise<Rental> {
+    const rental = await this.repository.create({
+      car_id,
+      expected_return_date,
+      user_id
+    })
+
+    await this.repository.save(rental);
+
+    return rental;
+  }
+}
+
+export { RentalsRepository }
