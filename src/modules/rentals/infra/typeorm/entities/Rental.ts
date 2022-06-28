f074d7aa1b746b5ea547b842e962da3f592a2d7a
pdavinunes <p.davi.nunes@gmail.com>
2022-06-27 22:27:51 -0300

    chore(aluguel): add implementação do typeorm

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,22 +1,34 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid'
 
+@Entity("rentals")
 class Rental {
+
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   car_id: string;
 
+  @Column()
   user_id: string;
 
+  @Column()
   expected_return_date: Date;
 
+  @Column()
   start_date: Date;
 
+  @Column()
   end_date: Date;
 
+  @Column()
   total: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @DeleteDateColumn()
   updated_at: Date;
 
   constructor() {
