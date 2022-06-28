03393aeaa75a25b3157c8898f03a0e3e88aedc45
pdavinunes <p.davi.nunes@gmail.com>
2022-06-27 22:30:30 -0300

    feat(aluguel): implementa controller de criação

---
+++ src/modules/rentals/useCases/createRental/CreateRentalController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+class CreateRentalController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { expected_return_date, car_id } = request.body;
+    const { id } = request.user
+
+    const createRentalUseCase = container.resolve(CreateRentalUseCase);
+
+    const rental = await createRentalUseCase.execute({
+      user_id: id,
+      expected_return_date,
+      car_id
+    })
+
+    return response.send(201).json(rental)
+  }
+}
+
+export { CreateRentalController }
