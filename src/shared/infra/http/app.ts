aa7c813571a4ec823d53546ec5a6bce29e3318a4
pdavinunes <p.davi.nunes@gmail.com>
2022-09-24 14:56:09 -0300

    chore: add sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,6 +5,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors'
 import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
+import * as Sentry from '@sentry/node'
+import * as Tracing from '@sentry/tracing'
 
 import { router } from './routes';
 import swaggerFile from '../../../swagger.json'
@@ -22,7 +24,20 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app })
+  ],
+  tracesSampleRate: 1.0
+})
+
+app.use(Sentry.Handlers.requestHandler())
+app.use(Sentry.Handlers.tracingHandler())
+
 app.use(express.json());
+
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
@@ -31,6 +46,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler())
 
 app.use((err: Error, _request: Request, response: Response, _next: NextFunction) => {
   if (err instanceof AppError) {
